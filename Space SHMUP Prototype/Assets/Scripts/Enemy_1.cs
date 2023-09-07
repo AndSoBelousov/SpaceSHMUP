3d2999e2267fa1ee82444a598acdda3055fb6f44
Andrey <124515352+AndSoBelousov@users.noreply.github.com>
2023-09-07 21:09:15 +0300

    Добавление логики врагов и начата работа над новыми оружием

--- Space SHMUP Prototype/Assets/Scripts/Enemy_1.cs
+++ Space SHMUP Prototype/Assets/Scripts/Enemy_1.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_1 : MonoBehaviour
+public class Enemy_1 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set in inspector: Enemy_1")]
+    //число сек полного цикла синусоиды
+    public float waveFrequency = 2;
+    //ширина синусоиды в метрах
+    public float waveWidth = 4;
+    public float waveRotY = 4;
+
+    private float xZero; // начальное значение координаты Х
+    private float birthTime;
+
+    private void Start()
     {
-        
+        xZero = pos.x;
+
+        birthTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Move()
     {
-        
+        Vector3 tempPos = pos;
+
+        float age = Time.time - birthTime;
+        float theta = Mathf.PI * 2 * age / waveFrequency;
+        float sin = Mathf.Sin(theta);
+        tempPos.x = xZero + waveWidth * sin;
+        pos = tempPos;
+
+        //повернуть немного относительно оси Y
+        Vector3 rot= new Vector3(0,sin*waveRotY,0);
+        this.transform.rotation = Quaternion.Euler(rot);
+
+        //движение вниз из родительского класса 
+        base.Move();
+
+        //print(boundsCheck.isOnScreen);
     }
 }
---
+++ Space SHMUP Prototype/Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using OpenCover.Framework.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public enum WeaponType
+{
+    none, // По умолчанию / нет оружия
+    blaster, // Простой бластер
+    spread, // Веерная пушка, стреляющая несколькими снарядами
+    phaser, // [Doit] Волновой фазер
+    missile, // [Doit] Самонаводящиеся ракеты
+    laser, // [Doit] Наносит повреждения при долговременном воздействии
+    shield // Увеличивает shieldLevel
+}
+
+[System.Serializable]
+public class WeaponDefinition
+{
+
+}
+
+public class Weapon : MonoBehaviour
+{
+ // Определение класса Weapon будет добавлено далее 
+
+}
